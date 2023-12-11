a5e0f7393193d41925a74e38ad1b7b813a908c4e
Lareb <lareb131213@gmail.com>
2023-12-11 19:53:51 +0530

    Your commit message here

--- src/main/java/com/email/Controller/EmailController.java
+++ src/main/java/com/email/Controller/EmailController.java
@@ -5,15 +5,18 @@ package com.email.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.email.model.EmailRequest;
+import com.email.model.EmailResponse;
 import com.email.service.EmailService;
 
 @RestController
+@CrossOrigin
 public class EmailController {
 
 
@@ -32,9 +35,9 @@ public class EmailController {
     public ResponseEntity<?> sendEmail(@RequestBody EmailRequest emailRequest){
         boolean result = this.emailService.sendEmail(emailRequest.getSubject(), emailRequest.getMessage(), emailRequest.getTo());
         if(result){
-            return ResponseEntity.ok("Email is sent successfully");
+            return ResponseEntity.ok(new EmailResponse("Email sent successfully"));
         }else{
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Email not sent");
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new EmailResponse("Email not sent"));
         }
         
         
